62bdb9130f136659b62a3f722550fc4071a5a5a4
SanchezsX <113286578+SanchezsX@users.noreply.github.com>
2022-09-17 02:00:44 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,128 @@
+// Timer field
+const hourElement = document.querySelector('.hour')
+const minuteElement = document.querySelector('.minute')
+const secondElement = document.querySelector('.second')
+const millisecondElement = document.querySelector('.millisecond')
+
+// Bottons
+const startButton = document.querySelector('.start')
+const pauseButton = document.querySelector('.pause')
+const stopButton = document.querySelector('.stop')
+const NewButton = document.querySelector('.New')
+
+// Listeners
+startButton.addEventListener('click', () => {
+    clearInterval(interval)
+    interval = setInterval(starTimer, 10)
+})
+
+pauseButton.addEventListener('click', () => {
+    clearInterval(interval)
+})
+
+stopButton.addEventListener('click', () => {
+    clearInterval(interval)
+    clearFields()
+
+    disableBtn()
+    hour = 00,
+    minute = 00,
+    second = 00,
+    millisecond = 00,
+    hourElement.textContent = "00"
+    minuteElement.textContent = "00"
+    secondElement.textContent = "00"
+    millisecondElement.textContent = "00"
+})
+
+NewButton.addEventListener('click', () => {
+    clearInterval(interval)
+    counter++
+    const results = document.querySelector('.results')
+    const block = document.createElement('div')
+    block.classList.add('results__info')
+    block.innerText = `Result: ${counter} ${hour}:${minute}:${second}:${millisecond}`
+    results.append(block)
+    clearFields()
+    clearInterval(interval)
+    interval = setInterval(starTimer, 10)
+    
+
+})
+
+// Variables
+let hour = 00,
+    minute = 00,
+    second = 00,
+    millisecond = 00,
+    interval
+    counter = 0
+    disabled = true
+
+
+
+
+function starTimer() {
+    millisecond++
+
+    // Millisecond
+    if(millisecond < 9) {
+        millisecondElement.innerText = "0" + millisecond
+    }
+    if(millisecond > 9) {
+        millisecondElement.innerText = millisecond
+    }
+    if (millisecond > 99) {
+        second++
+        secondElement.innerText = "0" + second
+        millisecond = 0
+        millisecondElement.innerText = "0" + millisecond
+    }
+
+    // Seconds
+    if(second < 9 ) {
+        secondElement.innerText = "0" + second
+    }
+    if(second > 9) {
+        secondElement.innerText = second
+    }
+    if(second > 59) {
+        minute++
+        minuteElement.innerText = "0" + minute
+        second = 0
+        secondElement.innerText = "0" + second
+
+    }
+
+    //Minutes
+     if (minute > 9) {
+        minuteElement.innerText = minute
+     }
+
+
+    //Hours
+    if (hour > 9) {
+        minuteElement.innerText = hour
+     }
+
+     NewButton.disabled = false
+}
+
+
+function clearFields() {
+    hour = 00,
+    minute = 00,
+    second = 00,
+    millisecond = 00,
+    hourElement.textContent = "00"
+    minuteElement.textContent = "00"
+    secondElement.textContent = "00"
+    millisecondElement.textContent = "00"  
+}
+
+function disableBtn() {
+    if(disabled) {
+        NewButton.disabled = true
+    }
+}
+disableBtn()
